2f8e4c100a24709b472e98b1b553c87becd01b9e
edielsontube <edielsontube@gmail.com>
2021-03-29 10:45:52 -0300

    first commit

---
+++ config.js
@@ -0,0 +1,140 @@
+const ownerNumber = ["557187645787@s.whatsapp.net"] 
+// mude somente o numero e não tire o @s.whatsapp.net
+
+const OriginalOwner = '557187645787' 
+// mude para seu numero
+
+const cr = 'EDIELSONTUBE 𝐁𝐎𝐓\n' 
+// texto do simbolo de verificado
+
+const prefix = '' 
+// prefixo
+
+
+// MENSAGENS DE BAN
+
+const banmsgtype = '*Corra membro comum, o ban estar próximo*'
+// mensagem de ban no anti tipos de mensagem
+
+const adminmsgtype = '*Tu é adm ent n vou te banir por mandar um tipo de mensagem proibida*'
+// mensagem de quando adm manda tipos de mensagens proibidas
+
+const banmsgporn = 'Hmmm pornozinho com antiporn ativado, ja sabe né...'
+// mensagem de ban no antiporn
+
+const adminmsgporn = '*Tu é adm ent n vou te banir por mandar porn*'
+// mensagem de quando adm manda porn com antiporn ativado
+
+const banmsglink = ' '
+// mensagem de ban no antilink
+
+const adminmsglink = 'Isso é um link amigo... ah é tu é adm ent pode kkkk 🙃'
+// mensagem de quando adm manda link
+
+
+// MENSAGENS DE BOM DIA BOA TARDE E BOA NOITE
+
+const linkimgday = 'https://i.imgur.com/RR18JiI.jpg'
+const textmsgday = 'Ohayo gozaimasu, Onii-chan 👉👈'
+// link e mensagem de bom dia
+
+const linkimgeve = 'https://i.ibb.co/bLXgcBW/lp-tarde-anime-by-hitsukinyan-db7fpqr-fullview.jpg'
+const textmsgeve = `Kon'nichiwa Onii-chan, aceite esse café ☕`
+// link e mensagem de boa tarde
+
+const linkimgnig = 'https://i.ibb.co/VJCd34F/EKvo372-X0-AEd-Hz.jpg'
+const textmsgnig = 'Konbanwa Onii-chan, espero que tenha tido um bom dia ☺️'
+// link e mensagem de boa noite 
+
+// MENSAGENS DE ERRO
+
+const msgerr = 'Deu erro, tente novamente :/'
+// mensagem de erro
+
+// MENSAGENS DE AUTOREPLY
+
+const botlindo = 'Lindo e o meu criador que me fez eu sou apenas um robo'
+const linkbotlindo = 'https://photos1.iorbix.com/00/00/00/00/02/72/43/64/C--6tZwaASQH-b.jpg'
+const botfeio = 'Sai dai bixa invejosa teu recalque nao me abala 😡'
+const cadebot = 'oque tu quer porra 👉👈?'
+const botfdp = 'Te foder rapaz, te deitar na porrada quando tiver dormindo'
+const botgostoso = 'Como vc sabe aonde foi que te comi 😳👉👈'
+const botfofo = 'fofo e minhas bola de baixo 😳👉👈'
+const botbaianor = 'Cala boca Boca de força'
+const botcorno = 'Vai te foder, jogador de ff, a tua mulher me chama de amor i eu q sou corno'
+const botputa = 'ta pensando que eu sou tu que transa d costa 😡'
+const botgay = 'aiiin bixa nao falem gay q eu fico toda derretida rosa shock meu bem para biba'
+const botviado = 'Viado eu nao sou mas pode me chamar de jumento 0 vc vai sentir a vara entrando no teu rego ate o talo'
+const numbotfeio = 'eu era feio ate te conhecer kkk '
+// KEYS
+
+const apiTobz = 'key'
+// pra pegar a key do Tobz va até o numero 
+//wa.me/447418349954 se registre no bot
+// e depois escreva #generateapikey que o 
+//bot do tobz vai gerar sua key
+
+// MENSAGENS DE EXCLUSIVIDADE
+let mess = {
+    wait: '⌛ Aguarde um pouco... ⌛ ENQUANTO ISSO INSCREVA-SE NO CANAL https://youtube.com/c/edielsontube',
+    success: '✔️ Sucesso! ✔️',
+    error: {
+        stick: '❌ Falha, ocorreu um erro ao converter a imagem em um adesivo ❌',
+        Iv: '❌ Link inválido ❌'
+    },
+    only: {
+        group: '❌ Este comando só pode ser usado em grupos! ❌',
+        ownerG: '❌ Este comando só pode ser usado pelo grupo proprietário! ❌',
+        ownerB: '❌ Este comando só pode ser usado pelo número proprietário! ❌',
+        admin: '❌ SILÊNCIO MEMBRO COMUM VC N TEM MORAL PRA USAR ESSE COMANDO ❌',
+        Badmin: '❌ Este comando só pode ser usado quando o bot se torna administrador! ❌'
+    }
+}
+
+// CONTATO DO CRIADOR
+
+const vcard = 'BEGIN:VCARD\n' 
++ 'VERSION:3.0\n' 
++ 'FN:Meu criador^~^\n' 
++ 'ORG:Ian;\n' 
++ 'TEL;type=CELL;type=VOICE;waid=559182487335:+55 91 8248-7335\n' 
++ 'END:VCARD'
+//vcard do propietário, mude os numeros de acordo com o formato que
+//ele corresponde
+
+
+// Nem ouse mexer aqui se n quiser que de erro
+
+exports.linkbotlindo = linkbotlindo
+exports.botlindo = botlindo
+exports.botfeio = botfeio
+exports.cadebot = cadebot
+exports.botfdp = botfdp
+exports.botgostoso = botgostoso
+exports.botfofo = botfofo
+exports.botbaianor = botbaianor
+exports.botcorno = botcorno
+exports.botputa = botputa
+exports.botgay = botgay
+exports.botviado = botviado
+exports.numbotfeio = numbotfeio
+exports.linkimgday = linkimgday
+exports.textmsgday = textmsgday
+exports.textmsgnig = textmsgnig
+exports.linkimgnig = linkimgnig
+exports.textmsgeve = textmsgeve
+exports.linkimgeve = linkimgeve
+exports.adminmsgtype = adminmsgtype
+exports.adminmsgporn = adminmsgporn
+exports.adminmsglink = adminmsglink
+exports.banmsglink = banmsglink
+exports.banmsgporn = banmsgporn
+exports.banmsgtype = banmsgtype
+exports.msgerr = msgerr
+exports.mess = mess
+exports.apiTobz = apiTobz
+exports.prefix = prefix
+exports.cr = cr
+exports.vcard = vcard
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname} INCREVA-SE NO NOSSO CANAL https://youtube.com/c/edielsontube`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Tchau @${number}. Já vai tarde sua carniça 👋`
+}
+exports.bye = bye
